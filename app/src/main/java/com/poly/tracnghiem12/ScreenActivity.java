9a3039504f034f269a0849df0b54c13fc542f1db
ngocthom <truongthithom1999@gmail.com>
2020-07-18 08:38:49 +0700

    du_an_1

---
+++ app/src/main/java/com/poly/tracnghiem12/ScreenActivity.java
@@ -0,0 +1,14 @@
+package com.poly.tracnghiem12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen);
+    }
+}
