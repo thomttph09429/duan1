fa5c48f3946c68e8cbde1148979dd2a9b7ca9ff6
ngocthom <truongthithom1999@gmail.com>
2020-08-08 13:55:48 +0700

    du_an_1

--- app/src/main/java/com/poly/tracnghiem12/Fragment/Trangchu_Fragment.java
+++ app/src/main/java/com/poly/tracnghiem12/Fragment/Trangchu_Fragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,7 +19,7 @@ import com.poly.tracnghiem12.R;
 
 
 public class Trangchu_Fragment extends Fragment {
-    LinearLayout tienganh10, tienganh11, tienganh12;
+    Button tienganh10, tienganh11, tienganh12;
     int id;
 
 
@@ -34,6 +35,7 @@ public class Trangchu_Fragment extends Fragment {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setTitle("Hướng dẫn");
                 builder.setMessage(".Bạn có thể làm bài trắc nghiệm\n" +
                         "bằng cách chọn môn học rồi sau \n" +
                         "đó trả lời từng câu hỏi đước đưa ra\n" +
